80c0fc3e112c578e35f8223306e979c3a5ab726e
QualMinds <QualMinds@DESKTOP-FRQ690F>
2025-11-06 12:19:31 +0530

    EntiyFramework Setup

---
+++ QMPractice.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QMPractice.API.Modesls.Domain;
+
+namespace QMPractice.API.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+
+    }
+}
